5b20623e639bd4bae02fce7fe130c4c43ff21b87
yuanxin.qiu <yuanxin.qiu@dji.com>
2016-10-09 09:41:32 +0800

    bugfix git-archive when release_path is not exist

--- lib/capistrano/recipes/deploy/strategy/advance_remote_cache.rb
+++ lib/capistrano/recipes/deploy/strategy/advance_remote_cache.rb
@@ -28,11 +28,12 @@ module Capistrano
           command = "if [ -d #{repository_cache} ]; then " +
             "#{source.sync(revision, repository_cache)}; " +
             "else #{source.checkout(revision, repository_cache)}; fi"
-          scm_run(command)
+          scm_run command
         end
 
         def copy_repository_cache
           logger.trace "copying the cached version to #{configuration[:release_path]}"
+          scm_run "if [ ! -d #{configuration[:release_path]} ]; then mkdir -p #{configuration[:release_path]}; fi"
           run "cd #{repository_cache} && git archive #{revision} | tar -x -f - -C #{configuration[:release_path]} && #{mark}"
         end
 
